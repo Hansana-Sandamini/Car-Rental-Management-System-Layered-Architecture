2692f7b1a0cedba410d53f52c1b18ae6089fa254
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-02-11 11:28:29 +0530

    Added QueryDAO Interface and QueryDAOImpl Class

---
+++ src/main/java/lk/ijse/rdfcarrentals/dao/custom/QueryDAO.java
@@ -0,0 +1,6 @@
+package lk.ijse.rdfcarrentals.dao.custom;
+
+import lk.ijse.rdfcarrentals.dao.SuperDAO;
+
+public interface QueryDAO extends SuperDAO {
+}
---
+++ src/main/java/lk/ijse/rdfcarrentals/dao/custom/impl/QueryDAOImpl.java
@@ -0,0 +1,6 @@
+package lk.ijse.rdfcarrentals.dao.custom.impl;
+
+import lk.ijse.rdfcarrentals.dao.custom.QueryDAO;
+
+public class QueryDAOImpl implements QueryDAO {
+}
